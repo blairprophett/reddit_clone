43320e01aae59faa444198df7f90c7261d1af4be
blairprophett <blaircuny@gmail.com>
2014-05-13 12:27:50 -0700

    boostrap file and devise gem and such

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditClone::Application.config.secret_key_base = 'a0ca101643fd2fd2f981454c460c030a5660c7c0a6e8d65acee6e45cf66d161679ac28e7b8b7cb180974609e0e1fa43e49578b490235469d4fe1f6e2bfc2483f'
