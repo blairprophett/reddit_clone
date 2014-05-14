6fb278106f1deda98290420d0d31bcff3036dbe3
blairprophett <blaircuny@gmail.com>
2014-05-14 09:31:17 -0700

    comments and posts working together

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,48 @@
+class CommentsController < ApplicationController
+  before_filter :load_post
+
+  def index
+    @comments = @post.comments.all
+    @user = current_user
+  end
+
+  def show
+    @comment = @post.comments.find(params[:id])
+    @user = current_user
+  end
+
+  def new
+    @comment = @post.comments.new
+  end
+
+  def create
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_comments_path(@post)
+  end
+
+  private
+  def comment_params
+    params[:comment].permit(:body)
+  end
+
+  def load_post
+    @post = Post.find(params[:post_id])
+  end
+
+end
+
+
+
+  
+
